45219a9b7c23175ad2de4b664188498f7804dbc9
damari <motokin128@gmail.com>
2021-06-22 22:54:12 +0900

    折れ線グラフに変更

--- app/javascript/packs/graph.js
+++ app/javascript/packs/graph.js
@@ -1,44 +1,49 @@
+import { Chart } from "chart.js/dist/chart";
+
 // turbolinksの読み込みを待つためのコードを追加しておく必要がある
 document.addEventListener("turbolinks:load", () => {
-  var ctx = document.getElementById("myChart").getContext("2d");
-  var myChart = new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [
-        {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
-          borderWidth: 1,
+  // 折れ線グラフのデータ
+  const lineLabel = ["1/1", "1/2", "1/4", "1/5", "1/6", "1/7"]
+  const lineData = [60.3, 61.1, 60.8, null, 60.5, 61.4]
+
+  // 折れ線グラフのオプション
+  const lineChartData = {
+    labels: lineLabel,
+    datasets: [{
+        label: "体重(kg)",
+        data: lineData,
+
+        // グラフの色を変更
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+        spanGaps: true
+      }]
+  }
+
+  const lineChartOption = {
+    title: {
+      display: true,
+      text: "ここにタイトルを記載できます"
+    },
+    tooltips: {
+      callbacks: {
+        // ホバー（スマホならタップ）時のラベル表示を変更
+        title: function (tooltipItems) {
+          return tooltipItems[0].xLabel.replace(/^(\d+).(\d+)$/, "$1月 $2月")
         },
-      ],
-    },
-    options: {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-            },
-          },
-        ],
-      },
-    },
-  });
-});
+        label: function (tooltipItems) {
+          return "体重: " + tooltipItems.yLabel + "kg"
+        }
+      }
+    }
+  }
+
+  // 折れ線グラフを表示
+  const lineChartContext = document.getElementById('line-chart').getContext('2d')
+  new Chart(lineChartContext, {
+    type: 'line',
+    data: lineChartData,
+    options: lineChartOption
+  })
+})
